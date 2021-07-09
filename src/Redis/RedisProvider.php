8699cb39147a7147a3143d0e2a04988802ad85ed
deno <inbox@email.com>
2021-07-09 14:05:28 +0800

    增加 Redis 类 select() 方法

--- src/Redis/RedisProvider.php
+++ src/Redis/RedisProvider.php
@@ -46,6 +46,7 @@ use Workerman\Worker;
  * @method static array keys($pattern)
  * @method static bool|array scan($it)
  * @method static void migrate($host, $port, $keys, $dbIndex, $timeout, $copy = false, $replace = false)
+ * @method static bool select($dbIndex)
  * @method static bool move($key, $dbIndex)
  * @method static string|int|bool object($information, $key)
  * @method static bool persist($key)
